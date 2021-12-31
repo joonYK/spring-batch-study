895133ef923ba7dd3f36ff70cc021028aa308ff6
JEFF <jeff@lific.io>
2021-12-31 22:02:58 +0900

    init

---
+++ src/test/java/jy/study/springbatch/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jy.study.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
