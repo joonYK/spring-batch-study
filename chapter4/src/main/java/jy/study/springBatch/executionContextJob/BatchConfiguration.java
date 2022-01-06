bd21b987e64c659056a3b1c6afaf92d9eefb2e03
JY <kims2606@gmail.com>
2022-01-06 22:54:44 +0900

    chapter4 - ExecutionContext 조작 - 스텝에서 잡으로 키 승격

--- chapter4/src/main/java/jy/study/springBatch/executionContextJob/BatchConfiguration.java
+++ chapter4/src/main/java/jy/study/springBatch/executionContextJob/BatchConfiguration.java
@@ -3,9 +3,11 @@ package jy.study.springBatch.executionContextJob;
 import jy.study.springBatch.executionContextJob.tasklet.HelloWorldTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +33,19 @@ public class BatchConfiguration {
     public Step step1() {
         return this.stepBuilderFactory.get("stop1")
                 .tasklet(new HelloWorldTasklet())
+                .listener(promotionListener())
                 .build();
+    }
+
+    /*
+     * StepExecution의 ExecutionContext에 있는 키를 JobExecution의 ExecutionContext로 승격할 때 사용.
+     * 스텝의 ExecutionContext에서 user.name 키를 찾으면 잡의 ExecutionContext에 복사.
+     */
+    @Bean
+    public StepExecutionListener promotionListener() {
+        ExecutionContextPromotionListener listener = new ExecutionContextPromotionListener();
+        listener.setKeys(new String[] {"user.name"});
+
+        return listener;
     }
 }
