08644d9dade692b06d50a431de1d24be48392ed7
JY <kims2606@gmail.com>
2022-01-08 23:30:17 +0900

    chapter4 - 다른 유형의 태스크릿 - MethodInvokingTasklet 파라미터 사용

--- chapter4/src/main/java/jy/study/springBatch/differentTasklet/MethodInvokingTaskletConfiguration.java
+++ chapter4/src/main/java/jy/study/springBatch/differentTasklet/MethodInvokingTaskletConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,26 +24,32 @@ public class MethodInvokingTaskletConfiguration {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job() {
+    public Job methodInvokingJob() {
         return this.jobBuilderFactory.get("methodInvokingJob")
-                .start(callableStep())
+                .start(methodInvokingStep())
                 .build();
     }
 
     @Bean
-    public Step callableStep() {
+    public Step methodInvokingStep() {
         return this.stepBuilderFactory.get("methodInvokingStep")
-                .tasklet(methodInvokingTasklet())
+                .tasklet(methodInvokingTasklet(null))
                 .build();
     }
 
+    @StepScope
     @Bean
-    public Tasklet methodInvokingTasklet() {
+    public Tasklet methodInvokingTasklet(
+            @Value("#{jobParameters['message']}") String message
+    ) {
 
         // 기존에 존재하던 다른 클래스 내의 메서드를 잡 내의 태스크릿처럼 사용 가능.
         MethodInvokingTaskletAdapter methodInvokingTaskletAdapter = new MethodInvokingTaskletAdapter();
+
         methodInvokingTaskletAdapter.setTargetObject(service());
         methodInvokingTaskletAdapter.setTargetMethod("serviceMethod");
+        methodInvokingTaskletAdapter.setArguments(new String[] {message});
+
         return methodInvokingTaskletAdapter;
     }
 
@@ -51,8 +59,8 @@ public class MethodInvokingTaskletConfiguration {
     }
 
     public static class CustomService {
-        public void serviceMethod() {
-            System.out.println("Service method was called");
+        public void serviceMethod(String message) {
+            System.out.println(message);
         }
     }
 }
