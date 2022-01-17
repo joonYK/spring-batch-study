1e9e9e7db82fcc0663ec8b30d553cf37a537e2f3
JY <kims2606@gmail.com>
2022-01-17 22:20:28 +0900

    chapter6 - rest 방식으로 잡 실행 구성 - 잡 파라미터 증가 (RunIdIncrementer) 적용

--- chapter6/src/main/java/jy/study/springBatch/rest/JobLaunchingController.java
+++ chapter6/src/main/java/jy/study/springBatch/rest/JobLaunchingController.java
@@ -3,6 +3,9 @@ package jy.study.springBatch.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +20,19 @@ public class JobLaunchingController {
 
     private final ApplicationContext context;
 
+    private final JobExplorer jobExplorer;
+
     @PostMapping("/run")
     public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
         Job job = this.context.getBean(request.getName(), Job.class);
-        return this.jobLauncher.run(job, request.getJobParameters()).getExitStatus();
+
+        //job에 RunIdIncrementer를 적용했기때문에 run.id라는 파라미터가 추가된 새로운 JobParameters 인스턴스 생성.
+        JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(), this.jobExplorer)
+                //Job이 JobParametersIncremeter를 가지고 있는지 해당 Job을 보고 판별.
+                //JobParametersIncremeter를 가지고 있으면 마지막 JobExecution에 사용됐던 JobParameters에 적용.
+                .getNextJobParameters(job)
+                .toJobParameters();
+
+        return this.jobLauncher.run(job, jobParameters).getExitStatus();
     }
 }
