edf9d527c8854d6727ca8c9d1579f8b0e9554462
JY <kims2606@gmail.com>
2022-01-19 23:14:26 +0900

    chapter6 - Quartz 방식으로 잡 실행 구성 주석 추가

--- chapter6/src/main/java/jy/study/springBatch/quartz/BatchScheduledJob.java
+++ chapter6/src/main/java/jy/study/springBatch/quartz/BatchScheduledJob.java
@@ -10,6 +10,10 @@ import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+/**
+ * 쿼츠 잡 정의.
+ * 일정 이벤트가 발생할 때 잡을 실행하는 매커니즘을 구현.
+ */
 @RequiredArgsConstructor
 public class BatchScheduledJob extends QuartzJobBean {
 
@@ -19,8 +23,12 @@ public class BatchScheduledJob extends QuartzJobBean {
 
     private final JobLauncher jobLauncher;
 
+    /**
+     * 스케줄링된 이벤트가 발생할 때마다 한 번씩 호출됨
+     */
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        //증분기를 적용한 jobParameters
         JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer)
                 .getNextJobParameters(this.job)
                 .toJobParameters();
--- chapter6/src/main/java/jy/study/springBatch/quartz/QuartzConfig.java
+++ chapter6/src/main/java/jy/study/springBatch/quartz/QuartzConfig.java
@@ -7,16 +7,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class QuartzConfig {
 
+    /**
+     * 쿼츠 잡의 빈을 구성
+     */
     @Bean
     public JobDetail quartzJobDetail() {
+        //쿼츠 잡 클래스를 정의한 BatchScheduledJob을 전달해서 생성.
         return JobBuilder.newJob(BatchScheduledJob.class)
                 .storeDurably()
                 .build();
     }
 
+    /**
+     * 트리거 생성.
+     * 스케줄과 JobDetail을 연관 지음.
+     */
     @Bean
     public Trigger jobTrigger() {
+        //스캐줄 생성 (얼마나 자주 JobDetail을 실행할 것인가)
         SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                //5초마다 한 번씩 잡을 수행하고, 최초 한번 수행 이후 4번 반복 수행. (총 5회 실행)
                 .withIntervalInSeconds(5).withRepeatCount(4);
 
         return TriggerBuilder.newTrigger()
