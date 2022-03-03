fda36c02270e6e33bba2fbe4a20048ea9e3ea141
JY <kims2606@gmail.com>
2022-03-03 23:08:55 +0900

    chapter10 - 배치 애플리케이션 예제 프로젝트 생성

---
+++ chapter10-exampleApplication/src/main/java/jy/study/springBatch/ExampleBatchApplication.java
@@ -0,0 +1,14 @@
+package jy.study.springBatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class ExampleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleBatchApplication.class, args);
+    }
+}
