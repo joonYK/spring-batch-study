f9e9d48572bca5cfa8fcc9408e44f47eae44b163
JY <kims2606@gmail.com>
2022-03-05 22:06:15 +0900

    chapter10 - 고객 정보 갱신 writer 작성. 첫 번째 스텝 완료(고객 정보 갱신).

--- chapter10-exampleApplication/src/main/java/jy/study/springBatch/ExampleBatchApplication.java
+++ chapter10-exampleApplication/src/main/java/jy/study/springBatch/ExampleBatchApplication.java
@@ -1,22 +1,15 @@
 package jy.study.springBatch;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
 @SpringBootApplication
-@RequiredArgsConstructor
 public class ExampleBatchApplication {
 
-    private final JobBuilderFactory jobBuilderFactory;
-
-    private final StepBuilderFactory stepBuilderFactory;
-
     public static void main(String[] args) {
-        SpringApplication.run(ExampleBatchApplication.class, args);
+        SpringApplication.run(ExampleBatchApplication.class,
+                "customerUpdateFile=/input/customer_update.csv");
     }
 }
--- chapter10-exampleApplication/src/main/java/jy/study/springBatch/job/ImportJobConfiguration.java
+++ chapter10-exampleApplication/src/main/java/jy/study/springBatch/job/ImportJobConfiguration.java
@@ -5,18 +5,23 @@ import jy.study.springBatch.domain.CustomerContactUpdate;
 import jy.study.springBatch.domain.CustomerNameUpdate;
 import jy.study.springBatch.domain.CustomerUpdate;
 import jy.study.springBatch.itemProcessor.CustomerItemValidator;
+import jy.study.springBatch.itemWriter.CustomerUpdateClassifier;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.batch.item.file.transform.PatternMatchingCompositeLineTokenizer;
+import org.springframework.batch.item.support.ClassifierCompositeItemWriter;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +29,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -140,6 +146,62 @@ public class ImportJobConfiguration {
         customerValidatingItemProcessor.setFilter(true);
 
         return customerValidatingItemProcessor;
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerNameUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET " +
+                        "FIRST_NAME = COALESCE(:firstName, FIRST_NAME)," +
+                        "MIDDLE_NAME = COALESCE(:middleName, MIDDLE_NAME)," +
+                        "LAST_NAME = COALESCE(:lastName, LAST_NAME) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerAddressUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET " +
+                        "ADDRESS1 = COALESCE(:address1, ADDRESS1), " +
+                        "ADDRESS2 = COALESCE(:address2, ADDRESS2), " +
+                        "CITY = COALESCE(:city, CITY)," +
+                        "STATE = COALESCE(:state, STATE)," +
+                        "POSTAL_CODE = COALESCE(:postalCode, POSTAL_CODE) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<CustomerUpdate> customerContactUpdateItemWriter(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<CustomerUpdate>()
+                .beanMapped()
+                .sql("UPDATE CUSTOMER SET " +
+                        "EMAIL_ADDRESS = COALESCE(:emailAddress, EMAIL_ADDRESS), " +
+                        "HOME_PHONE = COALESCE(:homePhone, HOME_PHONE), " +
+                        "CELL_PHONE = COALESCE(:cellPhone, CELL_PHONE), " +
+                        "WORK_PHONE = COALESCE(:workPhone, WORK_PHONE), " +
+                        "NOTIFICATION_PREF = COALESCE(:notificationPreferences, NOTIFICATION_PREF) " +
+                        "WHERE CUSTOMER_ID = :customerId")
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public ClassifierCompositeItemWriter<CustomerUpdate> customerUpdateItemWriter() {
+        CustomerUpdateClassifier classifier = new CustomerUpdateClassifier(
+                        customerNameUpdateItemWriter(null),
+                        customerAddressUpdateItemWriter(null),
+                        customerContactUpdateItemWriter(null));
+
+        ClassifierCompositeItemWriter<CustomerUpdate> compositeItemWriter = new ClassifierCompositeItemWriter<>();
+        compositeItemWriter.setClassifier(classifier);
+
+        return compositeItemWriter;
     }
 
 }
